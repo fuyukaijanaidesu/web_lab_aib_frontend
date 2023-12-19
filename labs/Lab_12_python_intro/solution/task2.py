195195b5343c6b3ccf132771e2fac08cfb28b2b5
fuyukaijanaidesu <101268482+fuyukaijanaidesu@users.noreply.github.com>
2023-12-19 23:52:37 +0300

    Update task2.py

--- labs/Lab_12_python_intro/solution/task2.py
+++ labs/Lab_12_python_intro/solution/task2.py
@@ -1,14 +1,19 @@
-length = int(input())
-sequence = []
-input = input().split()
-for i in range(length):
-    sequence.append(int(input[i]))
-medians = []
-for i in range(length):
-    sequence[:i + 1] = sorted(sequence[:i + 1])
-    if (i + 1) % 2 == 1:
-        median = sequence[(i + 1) // 2]
-    else:
-        median = sequence[i // 2]
-    medians.append(median)
-print(sum(medians))
+import bisect
+
+def median_sum(N, X):
+    result_sum = 0
+    sorted_sequence = []
+
+    for i in range(N):
+        current_element = X[i]
+        bisect.insort_left(sorted_sequence, current_element)
+        result_sum += sorted_sequence[i // 2]
+
+    return result_sum
+
+if __name__ == "__main__":
+    N = int(input())
+    X = list(map(int, input().split()))
+    result = median_sum(N, X)
+    print(result)
+
